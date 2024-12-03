7b34f777dce31f3c0e80ee35b0eae33567e10676
Ariel Ben-Yehuda <arielby@amazon.com>
2024-12-03 15:17:11 +0000

    test cleanup

--- fuzz/fuzz_targets/native_c.rs
+++ fuzz/fuzz_targets/native_c.rs
@@ -38,15 +38,22 @@ fn asciify(x: &str) -> String {
     result
 }
 
-fuzz_target!(|data: &[u8]| {
-    if data.len() == 0 {
-        return;
-    }
-    let alternate = data[0] % 2 == 0;
-    let data = &data[1..];
+fn fuzz(data: &[u8], alternate: bool) {
     let mut str_buf = String::with_capacity(16384);
     let mut buf = [0u8; 4096];
     let mut demangle = rustc_demangle_native_c::CDemangle::zero();
+    // We want to allow for easy overflow checking. The C output
+    // can be longer than the Rust output by a factor of up to *7/2,
+    // since e.g. an 'α' (U+03b1) in a constant string will be encoded
+    // as [b'\xce' b'\xb1'] in the C output (2 bytes) but as
+    // [b'\\' b'u' b'{' b'3' b'b' b'1' '}'] (7 bytes). The
+    // other factors are smaller than that (4 hex digits = 3 utf-8 bytes,
+    // leading to a lower expansion factor of *8/3, and so on).
+    //
+    // Also, to make the fuzzer more easily encounter overflow conditions
+    // in the C code, and since for most outputs the output lengths is the
+    // same, starting with a similar output length makes it easier.
+    let starting_buf_len = buf.len() / 4;
     let state;
     if let Ok(s) = std::str::from_utf8(data) {
         if let Ok(cs) = CString::new(data) {
@@ -55,7 +62,7 @@ fuzz_target!(|data: &[u8]| {
                 match rustc_demangle_native_c::rust_demangle_display_demangle(
                     &demangle,
                     buf.as_mut_ptr().cast(),
-                    buf.len() / 4,
+                    starting_buf_len,
                     alternate,
                 ) {
                     0 => {
@@ -77,7 +84,7 @@ fuzz_target!(|data: &[u8]| {
                     str_buf.clear();
                     let fmt_buf = &mut FormatBuf {
                         buf: &mut str_buf,
-                        max_len: buf.len() / 4 - 4,
+                        max_len: starting_buf_len - 4,
                     };
                     let rust_overflowed = if alternate {
                         write!(fmt_buf, "{:#}", rdemangle)
@@ -105,21 +112,21 @@ fuzz_target!(|data: &[u8]| {
                         }
                     };
                     assert_eq!(asciify(&str_buf), asciify(c_demangled));
-                    if c_demangled.len() < buf.len() / 4 - 3 {
+                    if c_demangled.len() < starting_buf_len - 3 {
                         panic!(
                             "spurious overflow {} {:?} {:?} {:?} {}",
                             c_demangled.len(),
                             alternate,
                             asciify(&str_buf),
                             asciify(c_demangled),
-                            buf.len() / 4
+                            starting_buf_len
                         )
                     }
                 }
                 State::Ok(demangled) => {
                     let fmt_buf = &mut FormatBuf {
                         buf: &mut str_buf,
-                        max_len: buf.len() / 4 - 4,
+                        max_len: starting_buf_len - 4,
                     };
                     let rust_overflowed = if alternate {
                         write!(fmt_buf, "{:#}", rdemangle)
@@ -137,4 +144,10 @@ fuzz_target!(|data: &[u8]| {
             }
         }
     }
+}
+
+fuzz_target!(|data: &[u8]| {
+    // fuzz both normal and alternate modes.
+    fuzz(data, false);
+    fuzz(data, true);
 });
